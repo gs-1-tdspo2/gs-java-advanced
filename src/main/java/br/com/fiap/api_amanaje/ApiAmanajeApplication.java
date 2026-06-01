fffc8012454668fe208f130ed5c22033da1b0509
GusCrevelari <gustavocrevelari@hotmail.com>
2026-06-01 17:47:30 -0300

    Spring Boot Amanaje initialization;

---
+++ src/main/java/br/com/fiap/api_amanaje/ApiAmanajeApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.api_amanaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAmanajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAmanajeApplication.class, args);
+	}
+
+}
